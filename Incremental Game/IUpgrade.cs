4f77919b2ec6cbf924be8011dca0d37adc0de633
Darius Wattimena <dariuswattimena@hotmail.com>
2016-07-06 13:23:18 +0200

    Added some new upgrades

---
+++ Incremental Game/IUpgrade.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Incremental_Game
+{
+    internal interface IUpgrade
+    {
+        string Name { get; set; }
+        string Description { get; set; }
+        double Price { get; set; }
+        double Profit { get; set; }
+        int Level { get; set; }
+    }
+}
--- Incremental Game/Upgrade.cs
+++ Incremental Game/Upgrade.cs
@@ -1,6 +1,9 @@
-﻿namespace Incremental_Game
+﻿using System.Collections.Generic;
+
+namespace Incremental_Game
 {
-    class Upgrade
+
+    internal class Upgrade : IUpgrade
     {
         public double GetUpgradePrice(string name, int level)
         {
@@ -8,8 +11,73 @@
             {
                 case "Click":
                     return 25 * level;
+                case "Shervin":
+                    return 100 * (level + 1);
+                case "Ruben":
+                    return 200 * (level + 1);
+                case "Frank":
+                    return 500 * (level + 1);
             }
             return 0;
         }
+
+        public Upgrade Upgrades(string name, int level)
+        {
+            switch (name)
+            {
+                case "Click":
+                    {
+                        return new Upgrade
+                        {
+                            Name = "Click Upgrade",
+                            Description = "Upgrade your clicking power.",
+                            Price = GetUpgradePrice(name, level),
+                            Profit = 1,
+                            Level = level
+                        };
+                    }
+                case "Shervin":
+                    {
+                        return new Upgrade
+                        {
+                            Name = "Shervin",
+                            Description = "Get some good profit from this fake black guy.",
+                            Price = GetUpgradePrice(name, level),
+                            Profit = 2.5,
+                            Level = level
+                        };
+                    }
+
+                case "Frank":
+                    {
+                        return new Upgrade
+                        {
+                            Name = "Frank",
+                            Description = "Every waifus will give you money since Frank is a young god.",
+                            Price = GetUpgradePrice(name, level),
+                            Profit = 10,
+                            Level = level
+                        };
+                    }
+                case "Ruben":
+                    {
+                        return new Upgrade
+                        {
+                            Name = "Ruben",
+                            Description = "This guy wil invent your money in csgo stickers for some easy profit.",
+                            Price = GetUpgradePrice(name, level),
+                            Profit = 5,
+                            Level = level
+                        };
+                    }
+            }
+            return null;
+        }
+
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public double Price { get; set; }
+        public double Profit { get; set; }
+        public int Level { get; set; }
     }
 }
